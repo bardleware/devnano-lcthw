31b6fb8f24777215aa4bed516944704edf0ff58d
bardleware <devan.sisson@vivintsolar.com>
2017-06-12 22:02:08 -0600

    deleted bin file, added hw

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 CFLAGS=-Wall -g 
-all: clean ex1 ex3 ex7 ex8
+all: clean ex1 ex3 ex7 ex8 ex9 ex10
 
 clean:
-	rm -f ex1 ex3 ex7 ex8 ex9
+	rm -f ex1 ex3 ex7 ex8 ex9 ex10
---
+++ wk2hw.c
@@ -0,0 +1,7 @@
+/*
+
+	Create a quiz application that ask you 10 questions.
+	After completing the 10 questions it will give you a score
+	(10/10) your percentage ( 100% ) and a letter grade (A,B,C,D,F).
+	
+*/
