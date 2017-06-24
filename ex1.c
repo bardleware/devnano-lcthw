3d14e8f7e846cd60422858da63f2037113b56ae0
devansisson <devancsis@gmail.com>
2017-06-23 20:08:30 -0600

    added installer for linux setup.

--- ex1.c
+++ ex1.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 
+/* multiline comment
+  still a multiline comment
+*/
+
 int main (int argc, char * argv[])
 {
-	char world[] = "World!";
+	char world[] = "World!";//added after 
+  int distance = 100;
+  int km = 30;
+  //single line comment
+
+  printf("You are %d miles away or %d kilometers.\n", distance, km);
 	
 	printf("Hello %s\n", world);
 
 	return 0;
 }
-/* multiline comment
-  still a multiline comment
-*/
-// int main(int argc, char *argv[])
-// {
-//   int distance = 100;
-//   int km = 30;
-//   //single line comment
-
-//   printf("You are %d miles away or %d kilometers.\n", distance, km);
-
-//   return 0;
-// }
---
+++ installer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+#http://software.opensuse.org/download.html?project=home%3AHorst3180&package=arc-theme
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/arc-theme.list"
+sudo apt-get update
+sudo apt-get install arc-theme
+
+#https://github.com/powerline/fonts
+
+#Numix:
+#https://github.com/numixproject/numix-icon-theme-shine
+#http://me4oslav.deviantart.com/art/Numix-Circle-Linux-Desktop-Icon-Theme-414741466
+
+#ZSH
+#https://gist.github.com/bardleware/be4292500baea132a62afc6b3115abd8
+#Gallois is the theme That I like for zsh
